47c4116aa5b41c7f084600f3be37bd005df6c979
hccazado <heitorcazado@gmail.com>
2021-11-10 21:05:49 -0300

    corrigindo lista carrinhoCOntroller, add campo qtd em detalhes de produto

--- Controllers/carrinhoController.js
+++ Controllers/carrinhoController.js
@@ -2,12 +2,13 @@ const session = require("express-session");
 
 const carrinho = {
     addItem: (req, res, next)=>{
-        let {id} = req.params
+        let {id} = req.params;
+        let {quantidade} = req.body;
         if(!req.session.carrinho){
             let carrinho = [];
             let produto = {
                 id: id,
-                quantidade: 1
+                quantidade: quantidade
             };
             carrinho.push(produto);
             req.session.carrinho = carrinho;
@@ -16,15 +17,14 @@ const carrinho = {
             return res.redirect("/produtos");
         }
         //variavel carrinho presente na session
-        else{
             //atribuindo conteudo da variavel carrinho da session a uma variavel
             let carrinho = req.session.carrinho;
             //recorrendo vetor do carrinho
             for(let i=0; i<carrinho.length; i++){
                 //verificando se o id do produto selecionado ja se encontra no array
                 if(carrinho[i].id == id){
-                    //array encontrado, incrementando valor da quantidade
-                    carrinho[i].quantidade ++;
+                    //array encontrado, alterando valor da quantidade
+                    carrinho[i].quantidade = quantidade;
                     //substituindo valor da variavel carrinho na session pela atual
                     req.session.carrinho = carrinho;
                     console.log("carrinho nao esta vazio. add mais um item");
@@ -36,8 +36,8 @@ const carrinho = {
             //Terminou de recorrer array e nao encontrou o id do produto selecionado
             //criando objeto do novo produto selecionado
             let produto = {
-                id: id,
-                quantidade: 1
+                id,
+                quantidade
             };
             //adicionado novo objeto ao vetor de produtos selecionados
             carrinho.push(produto);
@@ -47,7 +47,6 @@ const carrinho = {
             console.log(req.session.carrinho);
             //retornando pagina de produtos
             return res.redirect("/produtos");
-        }
     },
     removeItem: (req, res, next) =>{
 
--- Controllers/checkoutController.js
+++ Controllers/checkoutController.js
@@ -3,94 +3,87 @@ const { Result } = require("express-validator");
 
 const title = "lasCopas - Checkout";
 
-const {Produto, Uva, sequelize, Sequelize} = require('../models');
+const { Produto, Uva, sequelize, Sequelize } = require('../models');
 const Op = Sequelize.Op;
 
 //lista de objetos para testar!
 var produtos = [
-    {   id: 10,
+    {
+        id: 10,
         rotulo: "/images/uploads/rotulos/EM-roble.png",
         finca: "Estancia Mendoza",
         uvas: "merlot,malbec",
         valor: 79.90,
         qtd: 2,
-        total: ()=>this.valor*this.qtd
+        total: () => this.valor * this.qtd
     },
-    {   id: 12,
+    {
+        id: 12,
         rotulo: "/images/uploads/rotulos/Trapiche - cabernet.png",
         finca: "Trapiche",
         uvas: "Suavignon",
         valor: 83.90,
         qtd: 3,
-        total: ()=>this.valor*this.qtd
+        total: () => this.valor * this.qtd
     },
 ]
 
 
 
 const controller = {
-    carrinho: (req, res, next) =>{
-        
-        if(!req.session.carrinho){
-            res.render("carrinho", {
+    carrinho: (req, res, next) => {
+        //varifica se ja foi criado o atributo carrinho na session
+        if (!req.session.carrinho) {
+            return res.render("carrinho", {
                 title: title,
                 produtos: []
             })
         }
-
-        else{
-            let carrinho = req.session.carrinho;
-            produtos = [];
-            /*carrinho.forEach(item =>{
-                let resultado = Produto.findByPk(item.id,{
-                    include: Uva
-                }).then(resultado =>{
-                    let vinho = {...resultado.dataValues,
-                        qtd: item.quantidade,
-                        total: ()=>this.valor*this.qtd
-                    };
-                    let uvas = [];
-                    resultado.Uvas.forEach(uva=>{
-                        uvas.push(uva.dataValues.nome_uva);
-                    });
-                    vinho = {
-                        ...vinho,
-                        uvas
-                    };
-
-                    produtosArray.push(vinho);
-                    
-                    
-                });
-            });*/
-            
-            for(atual of carrinho){
-                Produto.findByPk(atual.id,{
-                    include: Uva
-                }).then(resultado =>{
-                    let vinho = {
-                        ...resultado.dataValues,
-                        qtd: atual.quantidade,
-                        total: ()=> this.valor * this.qtd
-                    }
-                    let uvas = [];
-                    for(uva of resultado.Uvas){
-                        uvas.push(uva.nome_uva);
-                    }
-                    vinho = {
-                        ...vinho,
-                        Uvas: uvas
-                    }
-
-                    //return vinho
-                    produtos.push(vinho);
-                })
-            }
-            console.log("saiu do for");
-
-            
-            
+        //Ação padrão, há produtos no carrinho da session
+        let carrinho = req.session.carrinho;
+        produtos = [];
+        
+        let listaId = [];
+        for (valor of carrinho) {
+            listaId.push(valor.id);
         }
+        Produto.findAll({
+            where: {
+                id_produto: {
+                    [Op.in]: listaId
+                }
+            },
+            include: [Uva]
+        }).then(resultado => {
+            let vinho = {};
+            resultado.forEach(atual =>{
+                let uvas = [];
+                atual.Uvas.forEach(uva =>{
+                    uvas.push(uva.nome_uva);
+                })
+                let quantidade =0;
+                for(aux of carrinho){
+                    if(aux.id == atual.dataValues.id_produto){
+                        quantidade = aux.quantidade;
+                    }
+                }
+                vinho = {
+                    ...atual.dataValues,
+                    qtd: quantidade,
+                    uvas: uvas,
+                    total: ()=> this.valor*this.qtd
+                }
+                
+                produtos.push(vinho);
+            });
+            //console.log(produtos);
+        }).then(_=>{
+            res.render("carrinho", {
+                title: title,
+                produtos: produtos
+            })
+        })
+
     },
     pagar: (req, res, next) => {
         res.render("pagamento", {
