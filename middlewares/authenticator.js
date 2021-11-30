15172694f9f166cb66a0d772d341c792ea83d68c
hccazado <heitorcazado@gmail.com>
2021-11-29 22:53:07 -0300

    implementado BootStrap view cadastroProduto

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -2,26 +2,6 @@ function autenticaLogin(req, res, next){
     //verifica se o objeto user na session é igual undefined(ainda nao atribuido)
     //e se há cookies de usuario
     //atribui os dados de sessao do cookie usuario à sessao
-    
-    /*
-    //REMOVENDO COOKIE para login
-    if(typeof(req.session.user) == "undefined" && req.cookies.user){
-        //console.log("Authenticator - encontrou cookie user e autenticou");
-        //console.log(req.cookies.user);
-        req.session.user = req.cookies.user;
-        return next();
-    }
-    //sessão indefinida e não há cookies de usuario
-    else if((typeof(req.session.user) == "undefined") && (!req.cookies.user)){
-        console.log("Authenticator - nao encontrou sessao nem cookie");
-        res.redirect("/login");
-    }
-    //existe sessão ativa, segue ao proximo passo
-    else if (typeof(req.session.user) != "undefined"){
-        console.log("Authenticator - sessão encontrada")
-        return next();
-    }*/
-
     //--------------------------------------------------
     //Não há sessão iniciada
     if(typeof(req.session.user) == "undefined"){ 
@@ -29,7 +9,6 @@ function autenticaLogin(req, res, next){
     }
     //existe sessão ativa, segue ao proximo passo
     else if (typeof(req.session.user) != "undefined"){
-        console.log("Authenticator - sessão encontrada")
         return next();
     }
 }
@@ -37,8 +16,6 @@ function autenticaLogin(req, res, next){
 function autenticaAdmin(req, res, next){
     //verifica se sessão está aberta e usuario é admin
     if(req.session.user){
-        //console.log("AutenticatorAdmin:dados da sessão:");
-        //console.log(req.session);
         if(req.session.user.admin == true){
             console.log("Authenticator, usuario admin");
             return next();
